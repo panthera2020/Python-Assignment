d6f20e35b6fc00c98dc3a2fc9327356c4b78cb2c
panthera2020 <diranoye2020@gmail.com>
2026-08-08 05:10:16 +0100

    modified atm simulator

--- Weekend Dose/atm_simulator.py
+++ Weekend Dose/atm_simulator.py
@@ -13,19 +13,28 @@ Balance -> Enter 3
 
 Exit -> Enter 0
 
-		"""))
+"""))
 
 	print()
 
 	match user_input:
 		case 1 : 
-			user_amount = int(input('Enter amount'))
-			balance += user_amount
+			user_amount = int(input('Enter amount: '))
+
+			print()
+
+			if user_amount > 0: balance += user_amount
+
+			elif user_amount < 0: print('Enter valid amount')
 		case 3 :
 			print('Your Balance: ', balance)
 		case 2 : 
-			user_withdrawal_amount = int(input('Enter amount'))
+			user_withdrawal_amount = int(input('Enter amount: '))
+
+			print()
+
 			if user_withdrawal_amount > balance : print('Insufficient Balance')
+			
 			elif user_withdrawal_amount <= balance :
 				balance -= user_withdrawal_amount
 				print('Transaction Complete!!!')
